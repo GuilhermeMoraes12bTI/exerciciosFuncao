9cc63f07b70001587bc6a51bbf9e3dd31e83e34a
guilhermemoraes <guilhermemoraesrodrigues2116@gmail.com>
2025-05-11 11:53:40 -0300

    resolução do exercicío 1005 - Média 1

---
+++ pages/media1/media1.js
@@ -0,0 +1,10 @@
+// arroq function
+let media = (nota1, nota2) => (( nota1 * 3.5) + (nota2 * 7.5)) / 11
+
+let media1 = media(5, 7.1)
+let media2 = media(0, 7.1)
+let media3 = media(10, 10)
+
+console.log(`Média 1 = ${media1.toFixed(5)}`)
+console.log(`Média 2 = ${media2.toFixed(5)}`)
+console.log(`Média 3 = ${media3.toFixed(5)}`)
